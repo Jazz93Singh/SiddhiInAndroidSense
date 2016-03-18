6459862bee84405394539287226975d4f7671c07
janithajc <jcsam93@gmail.com>
2016-03-18 15:03:15 +0530

    First commit

---
+++ app/src/main/java/org/wso2/edgeanalyticsservice/MainActivity.java
@@ -0,0 +1,23 @@
+package org.wso2.edgeanalyticsservice;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
